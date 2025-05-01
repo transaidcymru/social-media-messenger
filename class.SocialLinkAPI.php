d3926cad5c67b4e075bf953ab98390a6e9c3c05f
Trinity Lane <trinity@usepod.net>
2025-05-01 19:57:20 +0100

    Fix constructor

--- class.SocialLinkAPI.php
+++ class.SocialLinkAPI.php
@@ -2,6 +2,11 @@
 
 // Base class for social media API implementations (e.g. Facebook, BlueSky etc.).
 class SocialLinkAPI {
+
+    function __construct(){
+
+    }
+
     // TODO: Dummy implementation
     public function getMaxFetch() {
         return 0;
