70ab58a39e4a826dc12b804bade6345ece443c84
Kate <wimbleimble@protonmail.com>
2025-07-13 16:34:29 +0100

    shleep

--- webhook.php
+++ webhook.php
@@ -13,7 +13,14 @@ $hub_mode = $_GET["hub.mode"];
 $hub_challenge = $_GET["hub.challenge"];
 $hub_verify_token = $_GET["hub.verify_token"];
 
-if ($verify_token === $hub_verify_token)
+echo $hub_mode;
+echo $hub_challenge;
+echo $hub_verify_token;
+echo $verify_token;
+
+$result = strcmp($hub_verify_token, $verify_token);
+
+if ($result === 0)
 {
     Signal::send('smm.instagram-webhook', null,
         [
