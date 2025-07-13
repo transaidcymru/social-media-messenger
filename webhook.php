b996194a69acfa1f146914194917ac88a0559759
Kate <wimbleimble@protonmail.com>
2025-07-13 16:50:29 +0100

    maybe work?

--- webhook.php
+++ webhook.php
@@ -8,24 +8,20 @@ $verify_token = $config->get("instagram-verify-token");
 
 $body = json_decode(file_get_contents("php://input"));
 
-echo "hello world!!";
-
 $hub_verify_token = $body->hub->verify_token;
-
-echo $hub_verify_token;
-echo $verify_token;
-
-$result = strcmp($hub_verify_token, $verify_token);
+$hub_challenge = $body->hub->challenge;
+$hub_mode = $body->hub->mode;
 
 if ($result === 0)
 {
+    $data = [
+        "hub_mode" => $hub_mode,
+        "hub_challenge" => $hub_challenge,
+        "hub_verify_token" => $hub_verify_token
+    ];
     Signal::send('smm.instagram-webhook', null,
-        [
-            "hub_mode" => $hub_mode,
-            "hub_challenge" => $hub_challenge,
-            "hub_verify_token" => $hub_verify_token
-        ]);
-    //echo $hub_challenge;
+        $data);
+    echo $hub_challenge;
 }
 else
     echo "invalid verify token";
