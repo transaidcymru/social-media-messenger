765af2a1a392dfd534ad02681539a85b9973190a
Kate <wimbleimble@protonmail.com>
2025-07-13 16:28:24 +0100

    shboop

--- webhook.php
+++ webhook.php
@@ -1,28 +1,28 @@
 <?php
 require_once '../../../main.inc.php';
-//require_once INCLUDE_DIR.'class.signal.php';
-//require_once 'class.SocialLinkPlugin.php';
-//
-//$config = SocialLinkPlugin::getConfigStatic();
-//$verify_token = $config->get("instagram-verify-token");
-//
-//
-echo "hello world!!"
+require_once INCLUDE_DIR.'class.signal.php';
+require_once 'class.SocialLinkPlugin.php';
 
-//$hub_mode = $_GET["hub.mode"];
-//$hub_challenge = $_GET["hub.challenge"];
-//$hub_verify_token = $_GET["hub.verify_token"];
-//
-//if ($verify_token === $hub_verify_token)
-//{
-//    Signal::send('smm.instagram-webhook', null,
-//        [
-//            "hub_mode" => $hub_mode,
-//            "hub_challenge" => $hub_challenge,
-//            "hub_verify_token" => $hub_verify_token
-//        ]);
-//    echo $hub_challenge;
-//}
-//else
-//    echo "invalid verify token";
+$config = SocialLinkPlugin::getConfigStatic();
+$verify_token = $config->get("instagram-verify-token");
+
+
+echo "hello world!!";
+
+$hub_mode = $_GET["hub.mode"];
+$hub_challenge = $_GET["hub.challenge"];
+$hub_verify_token = $_GET["hub.verify_token"];
+
+if ($verify_token === $hub_verify_token)
+{
+    Signal::send('smm.instagram-webhook', null,
+        [
+            "hub_mode" => $hub_mode,
+            "hub_challenge" => $hub_challenge,
+            "hub_verify_token" => $hub_verify_token
+        ]);
+    echo $hub_challenge;
+}
+else
+    echo "invalid verify token";
 ?>
