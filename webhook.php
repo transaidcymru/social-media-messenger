04a1312e754f108a022b1b1fdf8a0d78557ad25c
Kate <wimbleimble@protonmail.com>
2025-07-13 16:23:46 +0100

    eep

--- webhook.php
+++ webhook.php
@@ -6,20 +6,23 @@ require_once 'class.SocialLinkPlugin.php';
 $config = SocialLinkPlugin::getConfigStatic();
 $verify_token = $config->get("instagram-verify-token");
 
-$hub_mode = $_GET["hub.mode"];
-$hub_challenge = $_GET["hub.challenge"];
-$hub_verify_token = $_GET["hub.verify_token"];
 
-if ($verify_token === $hub_verify_token)
-{
-    Signal::send('smm.instagram-webhook', null,
-        [
-            "hub_mode" => $hub_mode,
-            "hub_challenge" => $hub_challenge,
-            "hub_verify_token" => $hub_verify_token
-        ]);
-    echo $hub_challenge;
-}
-else
-    echo "invalid verify token";
+echo "hello world!!"
+
+//$hub_mode = $_GET["hub.mode"];
+//$hub_challenge = $_GET["hub.challenge"];
+//$hub_verify_token = $_GET["hub.verify_token"];
+//
+//if ($verify_token === $hub_verify_token)
+//{
+//    Signal::send('smm.instagram-webhook', null,
+//        [
+//            "hub_mode" => $hub_mode,
+//            "hub_challenge" => $hub_challenge,
+//            "hub_verify_token" => $hub_verify_token
+//        ]);
+//    echo $hub_challenge;
+//}
+//else
+//    echo "invalid verify token";
 ?>
