6ccccee288b8d686cf6df89bc780959993474647
Kate <wimbleimble@protonmail.com>
2025-07-30 20:16:59 +0100

    get request template

--- class.SocialLinkAPI.php
+++ class.SocialLinkAPI.php
@@ -7,6 +7,20 @@ class SocialLinkAPI {
         return array();
     }
 
+    public function get_request(string $endpoint, array $headers, array $params=null)
+    {
+        $url = $endpoint;
+        if ($params != null)
+        {
+            $url = $endpoint."?".http_build_query($params);
+        }
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
     public function sendMessage(string $conversation_id, string $message_content, &$error=null) {
         
     }
