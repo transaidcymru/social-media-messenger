ca45c5def6537b309546d2e7f9002e5f41fd3400
Kate <wimbleimble@protonmail.com>
2025-08-10 18:05:00 +0100

    configuration changes

--- config.php
+++ config.php
@@ -5,16 +5,23 @@ require_once INCLUDE_DIR . 'class.message.php';
 class SocialLinkPluginConfig extends PluginConfig {
     function getOptions() {
         return [
-            'instagram-api-key' => new TextboxField([
+            'instagram-api-key' => new PasswordField([
                 'default' => '',
                 'label' => 'Instagram API Key',
                 'hint' => 'API key for instagram API.',
             ]),
-            'instagram-verify-token' => new TextboxField([
+            'instagram-verify-token' => new PasswordField([
                 'default' => '',
                 'label' => 'Instagram API Webhook Key',
                 'hint' => 'Webhook secret set in admin settings to verify webhook requests.',
             ]),
+            // TODO: datetime field
+            'zero-hour' => new NumericField([
+                'default' => 0,
+                'label' => 'Zero hour',
+                'hint' => 'Unix time to scrape messages since',
+
+            ])
         ];
     }
 
