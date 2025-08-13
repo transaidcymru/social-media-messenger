435e8412f5b64a51c061b7fbd44177c8376511f1
Kate <wimbleimble@protonmail.com>
2025-08-13 19:59:01 +0100

    forgive me the autism is getting the better than me

--- class.SocialMediaMessage.php
+++ class.SocialMediaMessage.php
@@ -13,9 +13,8 @@ class SocialMediaMessage {
 
     public function encode()
     {
-        // todo: gaping security flaw.
         $time_formatted = date("Y-m-d H:i:s", $this->time);
-        return "<div id='message'> <script>alert('hehehe');</script><b>$time_formatted</b> $this->content</div>";
+        return "<div id='message' style='background-color: linear-gradient(90deg, #5BCEFA 0%, #5BCEFA 20%, #F5A9B8 20%, #F5A9B8 40%, #ffffff 40%, #ffffff 60%, #F5A9B8 60%, #F5A9B8 80%, #5BCEFA 80%, #5BCEFA 100%);'><b>$time_formatted</b>$this->content</div>";
     }
 }
 ?>
