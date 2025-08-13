1046f8e7fd6d9acf3218af4840c271cf0b962372
Kate <wimbleimble@protonmail.com>
2025-08-13 18:34:57 +0100

    sank

--- config.php
+++ config.php
@@ -5,12 +5,12 @@ require_once INCLUDE_DIR . 'class.message.php';
 class SocialLinkPluginConfig extends PluginConfig {
     function getOptions() {
         return [
-            'instagram-api-key' => new PasswordField([
+            'instagram-api-key' => new TextboxField([
                 'default' => '',
                 'label' => 'Instagram API Key',
                 'hint' => 'API key for instagram API.',
             ]),
-            'instagram-verify-token' => new PasswordField([
+            'instagram-verify-token' => new TextboxField([
                 'default' => '',
                 'label' => 'Instagram API Webhook Key',
                 'hint' => 'Webhook secret set in admin settings to verify webhook requests.',
