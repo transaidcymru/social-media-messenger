96c942dc6a6588f83b16ee133dc84bd6836a8a93
Kate <wimbleimble@protonmail.com>
2025-08-13 20:03:36 +0100

    it's been a while

--- class.SocialMediaMessage.php
+++ class.SocialMediaMessage.php
@@ -14,7 +14,7 @@ class SocialMediaMessage {
     public function encode()
     {
         $time_formatted = date("Y-m-d H:i:s", $this->time);
-        return "<div id='message' style='background-color: linear-gradient(90deg, #5BCEFA 0%, #5BCEFA 20%, #F5A9B8 20%, #F5A9B8 40%, #ffffff 40%, #ffffff 60%, #F5A9B8 60%, #F5A9B8 80%, #5BCEFA 80%, #5BCEFA 100%);'><b>$time_formatted</b>$this->content</div>";
+        return "<div id='message' style='background-image: linear-gradient(90deg, #5BCEFA 0%, #5BCEFA 20%, #F5A9B8 20%, #F5A9B8 40%, #ffffff 40%, #ffffff 60%, #F5A9B8 60%, #F5A9B8 80%, #5BCEFA 80%, #5BCEFA 100%);'><b>$time_formatted</b>$this->content</div>";
     }
 }
 ?>
