577e1d583b2fd48e67aa4a959d5ee077f0e48191
Trinity Lane <trinity@usepod.net>
2025-09-10 18:49:33 +0100

    Revert "NO webhook"
    This reverts commit 1b34352f88830ad9b90d6927aa5009e6804eb2f8.

---
+++ webhook.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../../../main.inc.php';
+require_once INCLUDE_DIR.'class.signal.php';
+require_once 'class.SocialLinkPlugin.php';
+
+$nothing = array();
+Signal::send('smm.sync', null, $nothing);
+echo "sinking!!!!!! AHHHHHH";
+
+//$config = SocialLinkPlugin::getConfigStatic();
+//$verify_token = $config->get("instagram-verify-token");
+//
+//$body = json_decode(file_get_contents("php://input"));
+//error_log(print_r($body, true));
+//
+////$hub_verify_token = $body->hub->verify_token;
+////$hub_challenge = $body->hub->challenge;
+////$hub_mode = $body->hub->mode;
+//$hub_verify_token = $_GET["hub_verify_token"];
+//$hub_challenge = $_GET["hub_challenge"];
+//$hub_mode = $_GET["hub_mode"];
+//
+//$result = strcmp($hub_verify_token, $verify_token);
+//
+//if ($result === 0)
+//{
+//    $data = [
+//        "hub_mode" => $hub_mode,
+//        "hub_challenge" => $hub_challenge,
+//        "hub_verify_token" => $hub_verify_token
+//    ];
+//    Signal::send('smm.instagram-webhook', null,
+//        $data);
+//    echo $hub_challenge;
+//}
+//else
+//    echo "invalid verify token";
+//
