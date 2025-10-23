d91ac1599733c36ceb18afd02c800d0c53cdee5a
Trinity Lane <trinity@usepod.net>
2025-10-23 20:51:25 +0100

    add timezone

--- APIClasses.php
+++ APIClasses.php
@@ -147,7 +147,7 @@ class SocialMediaMessage {
 
     public function encode()
     {
-        $time_formatted = Format::datetime($this->time, false);
+        $time_formatted = Format::datetime($this->time, false, $timezone = true);
         $imageString = join(array_map(fn ($m) => 
             "<details><summary style='cursor:pointer;text-decoration:underline;color:#5BCEFA'>Image Attachment</summary><figure><img src='cid:$m' data-image='$m' alt='image'/></figure></details>",
             $this->inlineImageIds));
