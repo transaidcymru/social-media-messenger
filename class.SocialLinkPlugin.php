d8f2481bbf5d104330f2ea5eb48c51cf52abbfab
Trinity Lane <trinity@usepod.net>
2026-01-15 19:45:12 +0000

    Log messages as debug

--- class.SocialLinkPlugin.php
+++ class.SocialLinkPlugin.php
@@ -352,6 +352,7 @@ class SocialLinkPlugin extends Plugin
 
             if ($error !== null) {
                 SCHLORP("GOD FUCKING DAMN IT. I nearly had it there (new session: ".($new_session ? "yes" : "no")."): \"$error\"");
+                SCHLORP("Messages: ".(print_r($messages, true)), SCHLORPNESS::DEBUG);
             }
         }
 
